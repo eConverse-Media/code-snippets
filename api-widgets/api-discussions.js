173b1cbcac8dd63bb7d7c2960abad78dda06d1b8
Nicola Hastings <nicolahastings@gmail.com>
2021-02-09 15:44:56 -0500

    NH | API discussions call

---
+++ api-widgets/api-discussions.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    class discussionList extends HTMLElement {
+        constructor() {
+            super();
+
+            var url = 'https://api.connectedcommunity.org/api/v2.0/Discussions/GetDiscussionPosts',
+                maxResults = this.getAttribute('max-results'),
+                discussionKey = this.getAttribute('discussion-key'),
+                includeStaff = this.getAttribute('include-staff'),
+                hasParams = false;
+
+            if (!!maxResults) {
+                url += (hasParams ? '&' : '?') + 'maxToRetrieve=' + maxResults;
+                hasParams = true;
+            }
+            if (!!discussionKey) {
+                url += (hasParams ? '&' : '?') + 'discussionKey=' + discussionKey;
+                hasParams = true;
+            }
+            if (!!includeStaff) {
+                url += (hasParams ? '&' : '?') + 'includeStaff=' + includeStaff;
+                hasParams = true;
+            }
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                datatype: 'json',
+                success: success,
+                headers: {
+                    'HLIAMKey': '78e2c103-9b11-4e73-bb3d-5ecb6cf4005c'
+                }
+            });
+
+            function success(resp) {
+                for (var i = 0; i < resp.length; i++) {
+
+                    var currentElem;
+                    $('<div class="discussion-post" />').appendTo('discussion-list');
+                    currentElem = $('discussion-list .discussion-post:last-child');
+                    $(currentElem).append('<h3><a href="' + resp[i].LinkToMessage + '">' + resp[i].DiscussionName + '</a></h3><h5><strong>By: </strong><a href=" ' + resp[i].Author.LinkToProfile + '">' + resp[i].Author.FirstName + ' ' + resp[i].Author.LastName + '</a></h5><h5><strong>Date:</strong> ' + resp[i].DatePosted + '</h5><div>' + resp[i].Body + '</div>');
+                }
+            }
+
+        }
+    }
+    customElements.define('discussion-list', discussionList);
+});

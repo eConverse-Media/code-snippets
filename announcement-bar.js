2ecc9083b800f6c1b76f6dbf26e371f9be01d9fb
Nicola Hastings <nicolahastings@gmail.com>
2021-02-23 16:37:21 -0500

    NH | updating announcement bar JS

--- announcement-bar.js
+++ announcement-bar.js
@@ -1,3 +1,19 @@
+function getCookies(cname) {
+    announcementBarCookies = [];
+    var name = cname;
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        announcementBarCookies.push(c);
+        }
+    }
+}
+
 function handleAnnouncementBar() {
     getCookies('announcementBar');
     var announcementBars = $('.announcement-bar').toArray();

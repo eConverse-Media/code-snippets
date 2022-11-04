dcc6a10344a370a7fc6a33f6b870932699f16fc6
Nicola Hastings <nicolahastings@gmail.com>
2022-11-04 13:14:38 -0400

    NH | updating background images JS to use aria-label instead of parent title attribute

--- background-images.js
+++ background-images.js
@@ -1,16 +1,15 @@
 // updated, more versatile option
 function handleBgImage(imgElem, elem) {
-    var img = $(imgElem).find('img:first-of-type').attr('src'),
-        url = "url('" + img + "')",
-        alt = $(imgElem).find('img:first-of-type').attr('alt');
+    var img = $(imgElem).find('img:first-of-type'),
+        src = $(img).attr('src'),
+        url = "url('" + src + "')";
 
     $(elem).css('background-image', url);
-    $(elem).attr('title', alt);
+    $(img).addClass('sr-only');
 }
 
 $(function () {
     $('.bg-image').each(function () {
         handleBgImage($(this), $(this));
-		$(this).find('img:first-of-type').hide();
     });
 });

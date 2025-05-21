f9e57fa1b26aa672a21c67ec808649a6fed9a64b
Nicola Hastings <nhastings@higherlogic.com>
2025-05-21 16:23:54 -0400

    NH | adding comment to copyright date JS to clarify how to use it

--- copyright-date.js
+++ copyright-date.js
@@ -1,4 +1,7 @@
 $(function() {
+
+    // This should be used in conjunction with a <span> tag with the class "copyright-year", placed where you want the date to display
+
     var year = new Date().getFullYear();
 
     $('.copyright-year').text(year);
